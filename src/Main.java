6193d586c1d3dcd814e47212ca5e675d3413b160
kleberbarros9 <napoleaok@hotmail.com>
2023-09-26 19:16:17 -0300

    Alterando no eclipse

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("OK!");
 		System.out.println("Alterado no git!");
+		System.out.println("Alterado no eclipse!");
 	}
 
 }
