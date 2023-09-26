a7dfa55c1e613a6849391dafa5e95cee465a3e01
kleberbarros9 <napoleaok@hotmail.com>
2023-09-26 20:07:56 -0300

    Incluindo imagens

--- src/tipoaluno/AlunoEspecial.java
+++ src/tipoaluno/AlunoEspecial.java
@@ -6,7 +6,7 @@ public class AlunoEspecial extends Aluno {
 	public AlunoEspecial() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public AlunoEspecial(double f) {
 		// TODO Auto-generated constructor stub
 	}
@@ -14,9 +14,11 @@ public class AlunoEspecial extends Aluno {
 	@Override
 	public void modificarNota(double n1, double n2, double n3, double n4) {
 		// TODO Auto-generated method stub
-		super.media = (((n1 + n2 + n3 + n4) / 4.0 * 1.75f));
+		double media = (((n1 + n2 + n3 + n4) / 4.0 * 1.75f));
+		if (media > 10.f)
+			media = 10.0f;
+
+		super.media = media;
 	}
-
-
 
 }
--- src/tipoaluno/AlunoRegular.java
+++ src/tipoaluno/AlunoRegular.java
@@ -13,7 +13,11 @@ public class AlunoRegular extends Aluno {
 	@Override
 	public void modificarNota(double n1, double n2, double n3, double n4) {
 		// TODO Auto-generated method stub
-		super.media = (((n1 + n2 + n3 + n4) / 4.0 * 1.25f));
+		double media = (((n1 + n2 + n3 + n4) / 4.0 * 1.25f));
+		if (media > 10.f)
+			media = 10.0f;
+
+		super.media = media;
 	}
 
 
